961b6886d8ec579dd3aec6f8cdc4e61d793a19be
randaliang <randaliang@126.com>
2018-10-11 09:57:31 +0800

    添加网关

--- springcloud-rdltest-gateway/src/main/java/com/superhope/SpringcloudRdltestGatewayApplication.java
+++ springcloud-rdltest-gateway/src/main/java/com/superhope/SpringcloudRdltestGatewayApplication.java
@@ -2,11 +2,14 @@ package com.superhope;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@SpringBootApplication
 @EnableZuulProxy
+@SpringCloudApplication
+
+@SpringBootApplication
 @EnableDiscoveryClient
 public class SpringcloudRdltestGatewayApplication {
 
